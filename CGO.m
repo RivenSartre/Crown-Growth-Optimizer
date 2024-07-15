b1da55697f38a0f3eea73185b38177728bc1e2ea
RivenSartre <74661424+RivenSartre@users.noreply.github.com>
2024-07-15 14:29:50 +0800

    Update CGO.m

--- CGO.m
+++ CGO.m
@@ -42,10 +42,15 @@ function [Best_pos,Best_fitness,Convergence_curve]=CGO(N,Max_iter,lb,ub,dim,fobj
         index(i) = i;
     end
     
-    Na = floor(N*0.382);          % Sprouting broach
-    Nb = N-Na;                    % Growing broach
+    Ns = floor(N*0.5);          % Sprouting broach
+    Ng = N-Ns;                    % Growing broach
     b  = 0.8;
+    Dis = (ub(1)-lb(1)) * 0.001;
+    alpha = 0.2;
     M(1) = 1;
+    distances = zeros(N,1);
+
+    X_temp = zeros(N,dim);
 
     %% main loop
     l = 2; 
@@ -64,34 +69,39 @@ function [Best_pos,Best_fitness,Convergence_curve]=CGO(N,Max_iter,lb,ub,dim,fobj
         
         %% growing and Sprouting
         
-        index_s = randperm(N,Na);
+        index_s = randperm(N,Ns);
         index_g = setdiff(index,index_s);
         
         % Sprouting
-        for i = 1:Na
+        for i = 1:Ns
             r1 = rand;
             k1 = randperm(5,1);
             for j = 1:size(X,2) 
-                idx=floor(rand(1)*N)+1;
                 X(index_s(i),j) = Pool(k1,j)+GR(index_s(i),j)*(r1*(Best_pos(j)-X(index_s(i),j))+(1-r1)*(X_centroid(j)-X(index_s(i),j)));
             end
         end
         
         % Growing
-        if Nb >= 1
-            for i = 1:Nb
+        if Ng >= 1
+            for i = 1:Ng
                 r2 = 2*rand-1;
                 for j = 1:size(X,2) 
-                    X(index_g(i),j) = X(index_g(i),j)+M(l)*GR(index_g(i),j)*(r2*(Best_pos(j)-X(index_g(i),j))+(1-r2)*(X_centroid(j)-X(index_g(i),j)));
+                    X_temp(index_g(i),j) = X(index_g(i),j)+M(l)*GR(index_g(i),j)*(r2*(Best_pos(j)-X(index_g(i),j))+(1-r2)*(X_centroid(j)-X(index_g(i),j)));
                 end
             end
+
+            for k=1:Ng
+                vec=zeros(N,1);
+                distances(k) = sqrt(sum((X_temp(index_g(i),j) - X(index_g(k), :)).^2));
+                if distances(k) < Dis
+                    vec(k) = X(index_g(k), j) - X_temp(index_g(i),j);
+                end
+                Freq = sum(vec);
+            end
+            X(index_g(i),j) =  X_temp(index_g(i),j) + Freq*alpha;
+
         end
         
-        % update Pruning 
-       if Na < N && mod(l,Max_iter/100)==0
-            Na = Na+1;
-            Nb = Nb-1;
-       end   
     
         %% Check to see if the solution is beyond the scope of the search and bring it back
         for i = 1:size(X,1)
@@ -113,20 +123,29 @@ function [Best_pos,Best_fitness,Convergence_curve]=CGO(N,Max_iter,lb,ub,dim,fobj
             end
         end
         
+        if Ns < N && mod(l,Max_iter/100)==0
+            Ns = Ns+1;
+            Ng = Ng-1;
+            [~, sortedIndices] = sort(Objective_values);
+            indices = sortedIndices(1:floor(N*0.382));
+            for k = 1:length(indices)
+                X(indices(k),:) = initialization_CGO(1,dim,ub,lb);      % initialization
+            end
+        end  
        
         %% update the pool
         [~,idx1] = sort(Objective_values);
         second_best = X(idx1(2),:);
         third_best = X(idx1(3),:);
-        sum1 = 0;
-        for i = 1:N_half
-            sum1 = sum1+X(idx1(i),:);
-        end
-        half_best_mean = sum1/N_half;
+        half = X(idx1(N_half),:);
+        % sum1 = 0;
+        % for i = 1:N_half
+        %     sum1 = sum1+X(idx1(i),:);
+        % end
         Pool(1,:) = Best_pos;
         Pool(2,:) = second_best;
         Pool(3,:) = third_best;
-        Pool(4,:) = half_best_mean;
+        Pool(4,:) = half;
         Pool(5,:) = mean(X);
 
         Convergence_curve(l) = Best_fitness;
